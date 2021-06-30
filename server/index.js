8e176db2b64d3ede5dd1b4c8a68a4a6e6cadc0cd
Miroslav Lysyuk <aldous@Miroslavs-MacBook-Pro.local>
2021-06-29 17:35:57 -0700

    changing terms to sad and happy

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const streamURL =
   'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
 
 // const rules = [{ value: `'${word1} OR ${word2}'`}]
-const rules = [{ value: 'art OR science'}]
+const rules = [{ value: 'sad OR happy'}]
 
 
 // Get stream rules
